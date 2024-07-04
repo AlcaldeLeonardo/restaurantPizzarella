63e3fe085f5344bf257a10440e1c68ff6b00c074
Leonardo Esteban Alcalde Gómez <108157788+AlcaldeLeonardo@users.noreply.github.com>
2024-07-04 08:15:03 -0300

    Pizzarella now has a about atribute and his get, fix method names too

--- src/modules/pizzarella.js
+++ src/modules/pizzarella.js
@@ -1,7 +1,10 @@
-export class PizarrelaInfo {
+export class PizzarelaInfo {
     static #name = "Pizzarella";
     static #description =
         "Experience authentic Italian pizza in every bite. Our traditional wood-fired oven and fresh ingredients will transport you to Naples. Discover the taste of Italy at Pizzarella";
+
+    static #about =
+        "In the charming restaurant Pizarrella, located in an Italian village, exquisite pizzas are prepared with a secret recipe passed down through generations. Each pizza reflects the passion and tradition of a family dedicated to Italian gastronomy. At Pizarrella, you will experience a unique culinary journey that transports you to the streets of Naples. Welcome to Pizarrella, where every bite is a taste of Italy on your palate!";
     static #hours = [
         {
             days: "Monday to Friday",
@@ -22,6 +25,9 @@ export class PizarrelaInfo {
     static get Description() {
         return this.#description;
     }
+    static get About() {
+        return this.#about;
+    }
 
     static get Hours() {
         let hours = this.#hours;
@@ -37,10 +43,10 @@ export class PizarrelaInfo {
         return this.#location;
     }
     static addDayToHours(day, hour) {
-        PizarrelaInfo.#hours.push({ day, hour });
+        this.#hours.push({ day, hour });
     }
 
     static updateDescription(newDescription) {
-        PizarrelaInfo.#description = newDescription;
+        this.#description = newDescription;
     }
 }
